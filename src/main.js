9e68e844c7bf8ed98605d3eeb4fd812b0b25e012
Aidan Gibson <tronicdude@gmail.com>
2023-11-16 12:40:34 -0800

    rm World.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,197 @@
-import { World } from './World/World.js'
 import Swal from 'sweetalert2'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { createCamera } from './World/components/camera.js'
+import { createLights } from './World/components/lights.js'
+import { createScene } from './World/components/scene.js'
+import { createRenderer } from './World/systems/renderer.js'
+import { Resizer } from './World/systems/Resizer.js'
+import { Loop } from './World/systems/Loop.js'
+import { Color, Scene } from 'three'
+
+
 let world
+let camera
+let controls
+let renderer
+let scene
+let loop
+let loader
+
+class World {
+	constructor(container) {
+		camera = createCamera()
+		renderer = createRenderer()
+		scene = createScene()
+		loop = new Loop(camera, scene, renderer)
+		container.append(renderer.domElement)
+
+
+
+		const controls = new OrbitControls(camera, renderer.domElement)
+
+		controls.enableDamping = true
+		controls.autoRotate = true
+		controls.autoRotateSpeed = 2.5
+		// controls.enableZoom = false;
+		// forward controls.update to our custom .tick method
+		controls.tick = () => controls.update()
+
+
+
+
+		const { ambientLight, mainLight } = createLights()
+		loader = new GLTFLoader()
+		loop.updatables.push(controls)
+		scene.add(ambientLight, mainLight)
+		this.A_DNA = null
+		this.B_DNA = null
+		this.Z_DNA = null
+		this.G_DNA = null
+
+		const resizer = new Resizer(container, camera, renderer)
+	}
+  
+	async init(dnaType) {
+		switch (dnaType) {
+		case 'A':
+			if (this.B_DNA) {
+				console.log('Removing B_DNA:', this.B_DNA)
+				scene.remove(this.B_DNA)
+			}
+			if (this.Z_DNA) {
+				console.log('Removing Z_DNA:', this.Z_DNA)
+				scene.remove(this.Z_DNA)
+			}
+			if (this.G_DNA) {
+				console.log('Removing G_DNA:', this.G_DNA)
+				scene.remove(this.G_DNA)
+			}
+			if (!this.A_DNA) {
+				let A_DNA_Data = await loader.loadAsync('/assets/models/A_DNA.glb')
+				this.A_DNA = A_DNA_Data.scene
+				//originally this was data.scene.children[0]; to just get first one
+				this.A_DNA.rotation.set(Math.PI, Math.PI, Math.PI / 2)
+			}
+			scene.add(this.A_DNA)
+			break
+		case 'B':
+			if (this.G_DNA) {
+				console.log('Removing G_DNA:', this.G_DNA)
+				scene.remove(this.G_DNA)
+			}
+			if (this.A_DNA) {
+				console.log('Removing A_DNA:', this.A_DNA)
+				scene.remove(this.A_DNA)
+			}
+			if (this.Z_DNA) {
+				console.log('Removing Z_DNA:', this.Z_DNA)
+				scene.remove(this.Z_DNA)
+			}
+			if (!this.B_DNA) {
+				let B_DNA_Data = await loader.loadAsync('/assets/models/B_DNA.glb')
+				this.B_DNA = B_DNA_Data.scene
+				this.B_DNA.rotation.set(Math.PI, Math.PI, Math.PI / 2)
+			}
+			scene.add(this.B_DNA)
+			break
+		case 'Z':
+			if (this.G_DNA) {
+				console.log('Removing G_DNA:', this.G_DNA)
+				scene.remove(this.G_DNA)
+			}
+			if (this.A_DNA) {
+				console.log('Removing A_DNA:', this.A_DNA)
+				scene.remove(this.A_DNA)
+			}
+			if (this.B_DNA) {
+				console.log('Removing B_DNA:', this.B_DNA)
+				scene.remove(this.B_DNA)
+			}
+			if (!this.Z_DNA) {
+				let Z_DNA_Data = await loader.loadAsync('/assets/models/Z_DNA.glb')
+				this.Z_DNA = Z_DNA_Data.scene
+				this.Z_DNA.rotation.set(Math.PI, Math.PI, Math.PI / 2)
+			}
+			scene.add(this.Z_DNA)
+			break
+		case 'G': // New case for 'G'
+			if (this.A_DNA) {
+				console.log('Removing A_DNA:', this.A_DNA)
+				scene.remove(this.A_DNA)
+			}
+			if (this.B_DNA) {
+				console.log('Removing B_DNA:', this.B_DNA)
+				scene.remove(this.B_DNA)
+			}
+			if (this.Z_DNA) {
+				console.log('Removing Z_DNA:', this.Z_DNA)
+				scene.remove(this.Z_DNA)
+			}
+			if (!this.G_DNA) {
+				let G_DNA_Data = await loader.loadAsync('/assets/models/G_DNA.glb')
+				this.G_DNA = G_DNA_Data.scene
+				this.G_DNA.rotation.set(Math.PI, Math.PI, Math.PI / 2)
+			}
+			scene.add(this.G_DNA)
+			break
+		default:
+			console.error('Invalid DNA type')
+		}
+	}
+  
+
+	// async init() {
+	//   const { A_DNA, B_DNA, Z_DNA } = await loadBirds();
+	//   scene.add(A_DNA);
+	//   scene.add(B_DNA);
+	//   scene.remove(B_DNA);
+
+	// move the target to the center of the front bird
+	// controls.target.copy(Z_DNA.position);
+
+	// scene.add(A_DNA, B_DNA);
+	// scene.add(B_DNA);
+	// scene.remove(A_DNA);
+
+	// adjust camera
+	// camera.position.set(-11.5, 1.5, 6.5);
+	// controls.update();
+	//controls.update() must be called after any manual changes to the camera's transform
+	// GUI
+	// const gui = new GUI()
+	// // 
+	// const cubeFolder = gui.addFolder('B-DNA')
+	// cubeFolder.add(B_DNA.rotation, 'x', 0, Math.PI * 2)
+	// cubeFolder.add(B_DNA.rotation, 'y', 0, Math.PI * 2)
+	// cubeFolder.add(B_DNA.rotation, 'z', 0, Math.PI * 2)
+	// cubeFolder.open()
+    
+
+	// const cameraFolder = gui.addFolder('Camera')
+	// cameraFolder.add(camera.position, 'z', 0, 10)
+	// // cameraFolder.add(camera.zoom, 'zoom')
+	// cameraFolder.open()
+	// }
+
+	render() {
+		renderer.render(scene, camera)
+	}
+
+	start() {
+		loop.start()
+
+	}
+
+	stop() {
+		loop.stop()
+	}
+}
+
+
+
+
 
 async function main() {
 	const container = document.querySelector('#scene-container')
